485b9f998be65a94da824105dad6a89f9254d1b9
chaos4ros2 <80691913+chaos4ros2@users.noreply.github.com>
2021-05-05 10:35:05 +0900

    Delete build directory

--- build/python_interface/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chaos/moveit2_ws/src/moveit2_tutorials/doc/moveit2_commander_ws/src/python_interface
-# Build directory: /home/chaos/moveit2_ws/src/moveit2_tutorials/doc/moveit2_commander_ws/build/python_interface
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/python_interface/ament_cmake_symlink_install_targets_0_.cmake
+++
@@ -1,1 +0,0 @@
-ament_cmake_symlink_install_targets("TARGET_FILES" "/home/chaos/moveit2_ws/src/moveit2_tutorials/doc/moveit2_commander_ws/build/python_interface/_core.so" "TARGETS" "_core" "ARCHIVE_DESTINATION" "lib/python_interface" "LIBRARY_DESTINATION" "lib/python_interface")
