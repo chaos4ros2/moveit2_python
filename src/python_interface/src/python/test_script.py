f6e7ad9abf9081569cfb9dfdab2e6fb1708609ec
chaos4ros2 <liuchao4664@yahoo.co.jp>
2021-05-10 23:15:55 +0900

    test pick.py

--- src/python_interface/src/python/test_script.py
+++ src/python_interface/src/python/test_script.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 from _core import *
+# test pick.py https://github.com/ros-planning/moveit2/blob/main/moveit_commander/demos/pick.py
+import sys
+import rclpy
+from moveit_commander import (
+    # RobotCommander,
+    # PlanningSceneInterface,
+)
+from geometry_msgs.msg import PoseStamped
 
-rclcpp_init()
-rclcpp_shutdown()
+if __name__ == "__main__":
+    rclcpp_init()
+    rclcpp_shutdown()
